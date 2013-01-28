e85ff79cfd1b38b3ca2169e37811b43c5a011951
Lawrence Kesteloot <lk@teamten.com>
2013-01-28 12:41:48 -0800

    Use goutil's sort utilities.

--- web.go
+++ web.go
@@ -9,6 +9,7 @@ import (
 	"code.google.com/p/go.net/websocket"
 	"encoding/json"
 	"fmt"
+	"github.com/lkesteloot/goutil/sort"
 	"github.com/lkesteloot/goutil/webutil"
 	"io/ioutil"
 	"log"
@@ -90,7 +91,7 @@ func generateFileList(w http.ResponseWriter, r *http.Request, dir, extension str
 
 	// The ReadDir() function returns the filenames sorted, but that puts "B12"
 	// before "B2".  So we re-sort taking into account numbers.
-	sortNumerically(filenames)
+	sort.SortNumerically(filenames)
 
 	// JSON-encoded.
 	w.Header().Set("Content-Type", "application/json")
